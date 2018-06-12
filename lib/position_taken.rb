e89133fb3a31d1fbe54e4059687e2ad47d3b1e94
Sushi Chanrai <sushi@pathworks.co>
2018-06-12 19:04:11 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,11 +4,11 @@ def position_taken?(board, index)
   # basic solution:
 
   taken = nil
-  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
-    taken = false
-  else
-  taken = true
-  end
+    if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+      taken = false
+    else
+    taken = true
+    end
   taken
 end
 
